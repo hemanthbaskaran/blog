8931caf3b2868a742003d09f9201e1185d2b2b63
Hemanth <146416256+hemanthbaskaran@users.noreply.github.com>
2024-11-16 22:02:26 +0530

    Update firebase.js

--- public/js/firebase.js
+++ public/js/firebase.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyAFIL8N-d5POTlzxZwg4C5xSJmU9KJujPY",
-    authDomain: "firestore-app-13292.firebaseapp.com",
-    projectId: "firestore-app-13292",
-    storageBucket: "firestore-app-13292.firebasestorage.app",
-    messagingSenderId: "265444500199",
-    appId: "1:265444500199:web:59af772fc030c4ba003842"
+    //use your api 
   };
   
   // Initialize Firebase
